d0d4652912ba747837ea239b046aab014cb42cf1
fanny00 <fannycamposa@gmail.com>
2023-02-19 11:37:48 -0300

    Servicio para buscar paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -8,11 +9,17 @@ import { Component } from '@angular/core';
 })
 export class PorPaisComponent {
 
-  termino: string = 'Hola Mundo';
+  termino: string = '';
 
-   constructor() {}
+   constructor( private paisService: PaisService) {}
 
    buscar() {
     console.log( this.termino );
+
+    this.paisService.buscarPais( this.termino )
+      .subscribe( resp => {
+        console.log(resp);
+      });
+
    }
 }
