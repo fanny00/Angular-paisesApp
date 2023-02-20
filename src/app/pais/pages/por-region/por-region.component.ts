a500f290b746572e19ad38b335c49910234aa132
fanny00 <fannycamposa@gmail.com>
2023-02-20 16:13:11 -0300

    CngClass, class

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -3,9 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [ `
+    button {
+      margin-right: 5px;
+    }
+    `
   ]
 })
 export class PorRegionComponent {
 
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
+
+  constructor() {}
+
+  activarRegion( region:string ){
+    this.regionActiva = region;
+
+    // TODO: hacer el llamado al servicio
+  }
 }
